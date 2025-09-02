906ccaae89ea79a1c27767752ec5ccf892371106
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-09-02 14:37:11 -0300

    first commit

---
+++ src/test/java/com/example/spring_boot_entidade_associada_para_um/SpringBootEntidadeAssociadaParaUmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_entidade_associada_para_um;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEntidadeAssociadaParaUmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
