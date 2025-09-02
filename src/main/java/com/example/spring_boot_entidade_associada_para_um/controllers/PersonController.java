c1cc4da142b23c8ad353429a14079b566e6050e2
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-09-02 16:54:15 -0300

    Save PersonDepartmentDTO

---
+++ src/main/java/com/example/spring_boot_entidade_associada_para_um/controllers/PersonController.java
@@ -0,0 +1,53 @@
+package com.example.spring_boot_entidade_associada_para_um.controllers;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.spring_boot_entidade_associada_para_um.dto.PersonDepartmentDTO;
+import com.example.spring_boot_entidade_associada_para_um.services.PersonService;
+
+@RestController
+@RequestMapping(value = "/people")
+public class PersonController {
+    @Autowired
+    private PersonService personService;
+
+    // @GetMapping(value = "/{id}")
+    // public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+    //     ProductDTO dto = productService.findById(id);
+    //     return ResponseEntity.ok(dto);
+    // }
+
+    // @GetMapping
+    // public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
+    //     Page<ProductDTO> dto = productService.findAll(pageable);
+    //     return ResponseEntity.ok(dto);
+    // }
+
+    @PostMapping
+    public ResponseEntity<PersonDepartmentDTO> insert(@RequestBody PersonDepartmentDTO dto) {
+        dto = personService.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    // @PutMapping(value = "/{id}")
+    // public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO dto)
+    //         throws InvalidDefinitionException {
+    //     dto = productService.update(id, dto);
+    //     return ResponseEntity.ok(dto);
+    // }
+
+    // @DeleteMapping(value = "/{id}")
+    // public ResponseEntity<Void> delete(@PathVariable Long id) {
+    //     productService.delete(id);
+    //     return ResponseEntity.noContent().build();
+    // }
+}
---
+++ src/main/java/com/example/spring_boot_entidade_associada_para_um/services/PersonService.java
@@ -0,0 +1,35 @@
+package com.example.spring_boot_entidade_associada_para_um.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.spring_boot_entidade_associada_para_um.dto.PersonDepartmentDTO;
+import com.example.spring_boot_entidade_associada_para_um.entities.Department;
+import com.example.spring_boot_entidade_associada_para_um.entities.Person;
+import com.example.spring_boot_entidade_associada_para_um.repositories.DepartmentRepository;
+import com.example.spring_boot_entidade_associada_para_um.repositories.PersonRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class PersonService {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    public PersonDepartmentDTO insert(PersonDepartmentDTO dto) {
+        Person entity = new Person();
+        entity.setName(dto.getName());
+        entity.setSalary(dto.getSalary());
+        Department dept = departmentRepository.findById(dto.getDepartment().getId()).orElseThrow(() -> new EntityNotFoundException("Departmento não encontrado"));
+        // Department dept = new Department();
+        // dept.setId(dto.getDepartment().getId());
+
+        entity.setDepartment(dept);
+        entity = personRepository.save(entity);
+        return new PersonDepartmentDTO(entity);
+    }
+}
